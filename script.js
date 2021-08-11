76b71ac9b1907840b3081be95b0014b67f19a812
Andrew McInally <realfunnyestate@yahoo.com>
2021-08-11 08:48:26 -0600

    color coding fixed

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
+//updating clock on top of page
+
 setInterval(function () {
   time = moment().local().format("dddd, MMM D, YYYY, h:mm:ssA");
   $("#currentTime").text(time);
 }, 1000);
+console.log("hello");
 
 $(document).ready(function () {
+  //save button event
   $(".saveBtn").on("click", function () {
-    var value = $(this).siblings(".description").val();
-    var time = $(this).parent().attr("id");
-    localStorage.setItem(time, value);
+    var inputVal = $(this).siblings(".description").val();
+    var timeSave = $(this).parent().attr("id");
+    //saves to locaL storage
+    localStorage.setItem(timeSave, inputVal);
   });
   function timeBlockColor() {
-    var hour = moment().hours();
+    var currentHour = moment().hour();
+
+    //loop hour slots
     $(".time-block").each(function () {
-      var slotHour = parseInt($(this).attr("id"));
-      if (slotHour < hour) {
+      var thisHour = parseInt($(this).attr("id"));
+
+      //if/else statements to check hour and assign background color
+      if (thisHour < currentHour) {
         $(this).removeClass("future");
         $(this).removeClass("present");
         $(this).addClass("past");
-      } else if (slotHour === hour) {
+      } else if (thisHour === currentHour) {
         $(this).removeClass("past");
         $(this).removeClass("future");
         $(this).addClass("present");
@@ -29,15 +38,15 @@ $(document).ready(function () {
     });
   }
 
-  timeBlockColor();
+  $("#9 .description").val(localStorage.getItem("hourNine"));
+  $("#10 .description").val(localStorage.getItem("hourTen"));
+  $("#11 .description").val(localStorage.getItem("hourEleven"));
+  $("#12 .description").val(localStorage.getItem("hourTweleve"));
+  $("#1 .description").val(localStorage.getItem("hourThirtee "));
+  $("#2 .description").val(localStorage.getItem("hourFourteen"));
+  $("#3 .description").val(localStorage.getItem("hourFifteen"));
+  $("#4 .description").val(localStorage.getItem("hourSixteen"));
+  $("#5 .description").val(localStorage.getItem("hourSeventeen"));
 
-  $("#hourNine .description").val(localStorage.getItem("hourNine"));
-  $("#hourTen .description").val(localStorage.getItem("hourTen"));
-  $("#hourEleven .description").val(localStorage.getItem("hourEleven"));
-  $("#hourTwelve .description").val(localStorage.getItem("hourTweleve"));
-  $("#hourThirteen .description").val(localStorage.getItem("hourThirtee "));
-  $("#hourFourteen .description").val(localStorage.getItem("hourFourteen"));
-  $("#hourFifteen .description").val(localStorage.getItem("hourFifteen"));
-  $("#hourSixteen .description").val(localStorage.getItem("hourSixteen"));
-  $("#hourSeventeen .description").val(localStorage.getItem("hourSeventeen"));
+  timeBlockColor();
 });
